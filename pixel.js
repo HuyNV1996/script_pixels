cc094a481cc2fac39309dd445927a661ade1303a
huynv <huynv@apec.com.vn>
2023-12-28 15:00:32 +0700

    initial

--- pixel.js
+++ pixel.js
@@ -4,6 +4,4 @@ window.addEventListener('load', function () {
     console.log(Shopify.Checkout.page)
     // For example, logging a message to the console
     console.log('The page has finished loading. Pixel.js is executed.');
-
-    // Your custom code can go here...
 });
