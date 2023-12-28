44d31f2d03b865a5c2d5788589d5adfb13448307
huynv <huynv@apec.com.vn>
2023-12-28 15:04:57 +0700

    Comment out logging of Shopify shop and checkout page.

--- pixel.js
+++ pixel.js
@@ -1,4 +1,4 @@
-console.log(Shopify.shop)
-console.log(Shopify.Checkout.page)
+// console.log(Shopify.shop)
+// console.log(Shopify.Checkout.page)
 // For example, logging a message to the console
 console.log('The page has finished loading. Pixel.js is executed.');
