16dad4468a03c83e17034e9c73b6d18bde2882a1
huynv <huynv@apec.com.vn>
2023-12-28 15:02:09 +0700

    Update pixel.js to log Shopify shop and checkout page information to the console.

--- pixel.js
+++ pixel.js
@@ -1,7 +1,4 @@
-window.addEventListener('load', function () {
-    // Code to be executed when the page finishes loading
-    console.log(Shopify.shop)
-    console.log(Shopify.Checkout.page)
-    // For example, logging a message to the console
-    console.log('The page has finished loading. Pixel.js is executed.');
-});
+console.log(Shopify.shop)
+console.log(Shopify.Checkout.page)
+// For example, logging a message to the console
+console.log('The page has finished loading. Pixel.js is executed.');
